0e950c524098af618a2565e4ecb66cfc0478a118
Dima Samoilik <103739636+dimadimaya@users.noreply.github.com>
2023-02-06 13:55:39 +0200

    hw7 phonebook with Redux Thunk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import styles from './App.module.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.phonebook}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <ContactList />
     </div>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,77 @@
+import styles from './ContactForm.module.css';
+import { useDispatch, useSelector } from 'react-redux/';
+import { addContact } from 'redux/operations';
+import { useState } from 'react';
+import shortid from 'shortid';
+import { selectContacts } from 'redux/selectors';
+
+export const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+
+  const handleChangeName = event => {
+    setName(event.currentTarget.value);
+  };
+
+  const handleChangeNumber = event => {
+    setNumber(event.currentTarget.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const normalizedName = name.toLowerCase();
+
+    if (
+      contacts.find(contact => contact.name.toLowerCase() === normalizedName)
+    ) {
+      return alert(`${name} is already in contacts`);
+    }
+    dispatch(
+      addContact({
+        id: shortid(),
+        name: name,
+        number: number,
+      })
+    );
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label>
+        Name
+        <input
+          value={name}
+          onChange={handleChangeName}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label>
+        Tel
+        <input
+          value={number}
+          onChange={handleChangeNumber}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button className={styles.btn} type="submit">
+        add contact
+      </button>
+    </form>
+  );
+};
